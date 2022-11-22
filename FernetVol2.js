fd4be6f1e7dcea63aadf519e4f9a658d7ee7e3bf
MatiGmnz <109489993+MatiGmnz@users.noreply.github.com>
2022-11-22 04:00:06 -0300

    Update FernetVol2.js

--- FernetVol2.js
+++ FernetVol2.js
@@ -7,25 +7,34 @@ let borrar = document.getElementById("delete");
 let cat = document.getElementById("kiti");
 let gato = document.getElementById("kat");
 let cierre = document.getElementById("cierro");
+let counterSuave = document.getElementById("counter-suave");
+let counterTranca = document.getElementById("counter-tranca");
+let counterFuerte = document.getElementById("counter-fuerte");
+let inicio1 = document.getElementById("suave");
+let inicio2 = document.getElementById("tranca");
+let inicio3 = document.getElementById("fuerte");
+let pausa1 = document.getElementById("pausa1");
+let pausa2 = document.getElementById("pausa2");
+let pausa3= document.getElementById("pausa3");
 
 
 element.addEventListener("click", () => { 
-   result.showModal(), calcular()});
+  result.showModal(), calcular()});
 
 borrar.addEventListener("click", () => {
   result.close()});
 
 function calcular() {
 
-var fernet = parseInt(marca.value);
-var recipiente = parseInt(texto.value);
-var fernet_branca = 35;
-var fernet_vittone = 40;
-var fernet_1882 = 32;
-var fernet_buhero_negro = 37;
-var fernet_artesanal = 39;
-var Coca = 100;
-var porcentaje_fernet;
+let fernet = parseInt(marca.value);
+let recipiente = parseInt(texto.value);
+let fernet_branca = 35;
+let fernet_vittone = 40;
+let fernet_1882 = 32;
+let fernet_buhero_negro = 37;
+let fernet_artesanal = 39;
+let Coca = 100;
+let porcentaje_fernet;
 if(fernet == 1)
 {
   porcentaje_fernet = fernet_branca * recipiente / Coca
@@ -45,7 +54,7 @@ if(fernet == 4)
 if (fernet == 5) {
   porcentaje_fernet = fernet_artesanal * recipiente / Coca
 };
- resultado.innerText = "Tu medida perfecta de fernet es: " + porcentaje_fernet + "ml."
+resultado.innerText = "Tu medida perfecta de fernet es: " + porcentaje_fernet + "ml."
 };
 
 cat.addEventListener("click", () => {
@@ -53,4 +62,95 @@ cat.addEventListener("click", () => {
 });
 
 cierre.addEventListener("click", () => {
-  kat.close()});
+  kat.close()
+});
+
+
+
+
+let cuentaRegresiva = document.createElement("h1");
+
+
+let timerSuave = 9;
+let timerTranca = 13;
+let timerFuerte = 17;
+
+let a1;
+let a2;
+let a3;
+
+let s = false;
+let t = false;
+let f = false;
+
+
+
+inicio1.addEventListener("click", () => {
+  let s = true;
+  
+  if(s === true){
+    a1 = setInterval(() => {
+      timerSuave--;
+      cuentaRegresiva.innerHTML = timerSuave;
+      counterSuave.appendChild(cuentaRegresiva);
+      if(timerSuave === 0){
+        clearInterval(a1);
+        cuentaRegresiva.innerHTML = "¡Tiempo!";
+        counterSuave.appendChild(cuentaRegresiva);
+        timerSuave = 9;
+      }
+    }, 1000);
+  }
+});
+
+inicio2.addEventListener("click", () => {
+  let t = true;
+
+  if(t === true){
+    a2 = setInterval(() => {
+      timerTranca--;
+      cuentaRegresiva.innerHTML = timerTranca;
+      counterTranca.appendChild(cuentaRegresiva);
+      if(timerTranca === 0){
+        clearInterval(a2);
+        cuentaRegresiva.innerHTML = "¡Tiempo!";
+        counterTranca.appendChild(cuentaRegresiva);
+        timerTranca = 13;
+      }
+    }, 1000);
+  }
+});
+
+inicio3.addEventListener("click", () => {
+  let f = true;
+
+  if(f === true){
+    a3 = setInterval(() => {
+      timerFuerte--;
+      cuentaRegresiva.innerHTML = timerFuerte;
+      counterFuerte.appendChild(cuentaRegresiva);
+      if(timerFuerte === 0){
+        clearInterval(a3);
+        cuentaRegresiva.innerHTML = "¡Tiempo!";
+        counterFuerte.appendChild(cuentaRegresiva);
+        timerFuerte = 17;
+      }
+    }, 1000);
+  }
+});
+
+
+pausa1.addEventListener("click", ()=>{
+  clearInterval(a1);
+  timerSuave = 9;
+});
+
+pausa2.addEventListener("click", ()=>{
+  clearInterval(a2);
+  timerTranca = 13;
+});
+
+pausa3.addEventListener("click", ()=>{
+  clearInterval(a3);
+  timerFuerte = 17;
+});
